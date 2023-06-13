51e9b6c4e4d6d49832163687d7ad1d57701b0f8f
Nika Gagua <nika.gagua@solutions2share.net>
2023-06-13 23:03:46 +0400

    Add project files.

---
+++ HotelBookingAPI/Controllers/HotelBookingController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HotelBookingAPI.Models;
+using HotelBookingAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookingAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HotelBookingController : ControllerBase
+    {
+        private readonly APIContext _context;
+        public HotelBookingController(APIContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public JsonResult CreateEdit(HotelBooking hotelBooking)
+        {
+            if (hotelBooking.Id == 0)
+            {
+                _context.Bookings.Add(hotelBooking);
+            }
+            else
+            {
+                var bookingInDb = _context.Bookings.Find(hotelBooking.Id);
+
+                if (bookingInDb == null)
+                {
+                    return new JsonResult(NotFound());
+                }
+
+                bookingInDb = hotelBooking;
+            }
+
+            _context.SaveChanges();
+
+            return new JsonResult(Ok(hotelBooking));
+        }
+
+        [HttpGet]
+        public JsonResult Get(int id)
+        {
+            var result = _context.Bookings.Find(id);
+
+            if (result == null)
+            {
+                return new JsonResult(NotFound());
+            }
+
+            return new JsonResult(Ok(result));
+        }
+
+        [HttpDelete]
+        public JsonResult Delete(int id)
+        {
+            var result = _context.Bookings.Find(id);
+
+            if (result == null)
+            {
+                return new JsonResult(NotFound());
+            }
+
+            _context.Bookings.Remove(result);
+            _context.SaveChanges();
+
+            return new JsonResult(NoContent());
+        }
+
+        [HttpGet]
+        public JsonResult GetAll()
+        {
+            var result = _context.Bookings.ToList();
+            return new JsonResult(result);
+        }
+    }
+}
---
+++ HotelBookingAPI/Data/APIContext.cs
@@ -0,0 +1,16 @@
+﻿using HotelBookingAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBookingAPI.Data
+{
+    public class APIContext : DbContext
+    {
+        public DbSet<HotelBooking> Bookings { get; set; }
+
+        public APIContext(DbContextOptions<APIContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
